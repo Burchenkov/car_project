1819b136e3117593f79b73c32657809984c5e44d
Y*********Antonio <dcas@mail.ru>
2024-04-17 05:12:58 +0300

    Merge branch 'main' of https://github.com/Burchenkov/car_project

--- new_bot.py
+++ new_bot.py
@@ -32,52 +32,6 @@ dp = Dispatcher()
 
 list = []
 
-# @dp.message(Command('start'))
-# async def send_welcome(message: types.Message):
-#     await message.answer("Привет! Я бот для ввода машин "
-#                          "Какую машину хотите добавить, напишите simple,или special? " 
-#                          "Если хотите выйти, наберите exit")
-
-
-    # @dp.message(Command('exit'))
-    # async def send_bye(message: types.Message):
-    #     await message.answer("Пока пока, заходи если что!")
-        
- 
-# @dp.message(Command('simple'))
-# async def send_simple(message: types.Message):
-#     marks = None
-#     yoc = None
-#     country = None
-    
-#     await message.answer("Вы добавили простую машину! Укажите марку: ")
-#     # list.append(Car(0,"", message.text))
-#     # print(list[0])
-#     @dp.message()
-#     async def data(message: types.Message, this_marks, yoc, country):
-#         this_marks = marks
-
-#         await message.reply(f"Вы добавили марку {marks}")
-
-        
-
-    # year = int(input("Введите год: \n"))
-    # country = input("Введите страну: \n")
-    # mark = input("Введите марку: \n")
-
-    # list.append( Car(year, country, mark) )
-
-    # print(list[0].country)
-
-
-# @dp.message(Command('special'))
-# async def send_special(message: types.Message):
-#     type = "special"
-
-
-# async def send_mark(message: types.Message):
-#     await message.reply("Введите марку")
-
 #------------------------------------------------------------------------
 @dp.message(Command('start'))
 async def start_adding_car(message: types.Message):
