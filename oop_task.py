ce3e97e892340dc5beb04f7b6677d11bbce104c6
mkvzlzz <smellkvellk75@gmail.com>
2024-04-18 20:53:25 +0300

    now its all done almost perfectly

--- oop_task.py
+++ oop_task.py
@@ -41,22 +41,6 @@ def add_car():
         specialization = input("Введите специализацию машины: ")
         main_car = New_car(car_id, year, country, model, carrying_capacity, specialization)
 
-    if car_type.lower() == "обычная":
-        car_id = len(cars) + 1
-        year = input("Введите год выпуска машины: ")
-        country = input("Введите страну выпуска машины: ")
-        model = input("Введите марку машины: ")
-        main_car = Car(car_id, year, country, model)
-
-    elif car_type.lower() == "инженерная":
-        car_id = len(cars) + 1
-        year = input("Введите год выпуска машины: ")
-        country = input("Введите страну выпуска машины: ")
-        model = input("Введите марку машины: ")
-        carrying_capacity = input("Введите грузоподъемность машины: ")
-        specialization = input("Введите специализацию машины: ")
-        main_car = New_car(car_id, year, country, model, carrying_capacity, specialization)
-
     else:
         print("Неверный тип машины. Попробуйте еще раз.")
         return
@@ -73,19 +57,9 @@ def view_cars():
         if car_type == "инженерные":
             unique_specializations = set([car.specialization for car in car_list.values()])
             print(f"Уникальные специализации {car_type} машин: {sorted(list(unique_specializations))}")
-    print(f"Количество машин: {len(cars)}")
-    unique_brands = set([car.model for car in cars.values()])
-    print(f"Уникальные марки машин: {sorted(list(unique_brands))}")
-
-    for car_type, car_list in cars.items():
-        if car_type == "инженерные":
-            unique_specializations = set([car.specialization for car in car_list.values()])
-            print(f"Уникальные специализации {car_type} машин: {sorted(list(unique_specializations))}")
 
 while True:
     print("\nМеню:")
-    print("1. Добавление машин.")
-    print("2. Вывести все машины.")
     print("1. Добавление машин.")
     print("2. Вывести все машины.")
     print("3. Программа завершена.")
